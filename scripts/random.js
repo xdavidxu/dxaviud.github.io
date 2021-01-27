d7cf19bdd9dbdaf7ca4dbef1c60ed467abae9b07
David Xu <dxaviud@gmail.com>
2021-01-27 13:24:36 -0500

    changed video

--- scripts/random.js
+++ scripts/random.js
@@ -24,7 +24,7 @@ image.onclick = function() {
 }
 
 const iframe = document.querySelector("iframe");
-iframe.setAttribute("src", "https://www.youtube.com/embed/GwaRztMaoY0");
+iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/Wni78jvgGao?mute=1");
 
 const changeNameButton = document.getElementById("change-name");
 changeNameButton.onclick = function() {
