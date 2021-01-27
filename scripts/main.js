07ffdb2bf56cd046908ec38cb22b48f88f8d9089
David Xu <dxaviud@gmail.com>
2021-01-27 12:48:12 -0500

    random page

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,5 @@
 console.log("Hello World!");
 
-const heading = document.querySelector("h1");
-
-function setName() {
-    const name = prompt("Please enter your name.");
-    if (!name) {
-        setName();
-    } else {
-        heading.textContent = name;
-    }
-}
-
-const image = document.querySelector("img");
-const figureCaption = document.querySelector("figcaption");
-image.onclick = function() {
-    const src = image.getAttribute("src");
-    if (src === "https://via.placeholder.com/1200x600") {
-        image.setAttribute("src", "https://via.placeholder.com/1200x800");
-        figureCaption.textContent = "Another placeholder image";
-    } else {
-        image.setAttribute("src", "https://via.placeholder.com/1200x600");
-        figureCaption.textContent = "Placeholder image";
-    }
-}
-
-const iframe = document.querySelector("iframe");
-iframe.setAttribute("src", "https://www.youtube.com/embed/GwaRztMaoY0");
-
-const changeNameButton = document.getElementById("change-name");
-changeNameButton.onclick = function() {
-    setName();
-}
-
 var slideIndex = 0;
 showVideo(slideIndex);
 
@@ -50,4 +18,10 @@ function showVideo(n) {
     }
     slides[slideIndex].style.display = "block";
     title.textContent = slides[slideIndex].textContent;
+}
+
+const toggler = document.getElementsByClassName("toggler");
+
+function closeHamburger() {
+    toggler[0].click();
 }
---
+++ scripts/random.js
@@ -0,0 +1,32 @@
+
+const heading = document.querySelector("h1");
+
+function setName() {
+    const name = prompt("Please enter your name.");
+    if (!name) {
+        setName();
+    } else {
+        heading.textContent = name;
+    }
+}
+
+const image = document.querySelector("img");
+const figureCaption = document.querySelector("figcaption");
+image.onclick = function() {
+    const src = image.getAttribute("src");
+    if (src === "https://via.placeholder.com/1200x600") {
+        image.setAttribute("src", "https://via.placeholder.com/1200x800");
+        figureCaption.textContent = "Another placeholder image";
+    } else {
+        image.setAttribute("src", "https://via.placeholder.com/1200x600");
+        figureCaption.textContent = "Placeholder image";
+    }
+}
+
+const iframe = document.querySelector("iframe");
+iframe.setAttribute("src", "https://www.youtube.com/embed/GwaRztMaoY0");
+
+const changeNameButton = document.getElementById("change-name");
+changeNameButton.onclick = function() {
+    setName();
+}
