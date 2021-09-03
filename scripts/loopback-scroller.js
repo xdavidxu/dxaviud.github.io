edd1c058b90914ef81c309f74dd0d31da12c308f
David Xu <62825020+dxaviud@users.noreply.github.com>
2021-09-02 23:10:51 -0400

    Implement pwards loopback scrolling (thx mom, couldn't have figured it out if you didn't talk to me about it)

--- scripts/loopback-scroller.js
+++ scripts/loopback-scroller.js
@@ -1,12 +1,20 @@
 const scrollers = document.getElementsByClassName("loopback-scroller");
 for (const scroller of scrollers) {
-    const n = scroller.children.length;
+    let n = scroller.children.length;
     const buffer =
         parseInt(scroller.getAttribute("scroller-buffer"), 10) || 100;
+
+    scroller.scrollTo(0, (scroller.scrollHeight - scroller.clientHeight) / 2);
 
     for (let i = 0; i < n; ++i) {
         const clone = scroller.children[i].cloneNode(true);
         scroller.appendChild(clone);
+    }
+    if (n === 1) {
+        // clone two extras if only 1 child, otherwise loopback won't work
+        scroller.appendChild(scroller.children[0].cloneNode(true));
+        scroller.appendChild(scroller.children[0].cloneNode(true));
+        n = 2; // pretend there was originally 2 children
     }
 
     scroller.addEventListener("scroll", () => {
@@ -14,10 +22,17 @@ for (const scroller of scrollers) {
             scroller.scrollTop >=
             scroller.scrollHeight - scroller.clientHeight - buffer
         ) {
-            for (let i = 0; i < n; ++i) {
+            for (let i = 0; i < n - 1; ++i) {
                 const first = scroller.children[0];
                 scroller.removeChild(first);
                 scroller.appendChild(first);
+            }
+        }
+        if (scroller.scrollTop <= buffer) {
+            for (let i = 0; i < n - 1; ++i) {
+                const last = scroller.children[2 * n - 1];
+                scroller.removeChild(last);
+                scroller.prepend(last);
             }
         }
     });
