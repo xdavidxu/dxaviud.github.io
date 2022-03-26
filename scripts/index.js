10839fc9ce8b2c01bc1dfe59e453e64e6664ee9f
David Xu <dxaviud@gmail.com>
2022-03-25 22:42:25 -0400

    apply prettier formatting, add bitgo bullets, tweak small stuff

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const toggler = document.querySelector(".toggler");
 // this takes advantage of event delegation (https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events)
 const menuItemsList = document.querySelector("ul.menu-items-list");
 menuItemsList.addEventListener("click", function () {
-    toggler.click();
+  toggler.click();
 });
 
 // Anti-Rona Lock System slides
@@ -18,36 +18,36 @@ showImage();
 
 let arlsLeftButton = document.querySelector(".arls-slides .slides-button.left");
 let arlsRightButton = document.querySelector(
-    ".arls-slides .slides-button.right"
+  ".arls-slides .slides-button.right"
 );
 arlsLeftButton.addEventListener("click", function () {
-    changeImage(-1);
+  changeImage(-1);
 });
 arlsRightButton.addEventListener("click", function () {
-    changeImage(+1);
+  changeImage(+1);
 });
 
 function changeImage(delta) {
-    if (Math.abs(delta) != 1) {
-        throw "Error: delta not 1 or -1";
-    }
-    arlsSlideIndex += delta;
-    if (arlsSlideIndex > arlsSlides.length - 1) {
-        arlsSlideIndex = 0;
-    } else if (arlsSlideIndex < 0) {
-        arlsSlideIndex = arlsSlides.length - 1;
-    }
-    showImage();
+  if (Math.abs(delta) != 1) {
+    throw "Error: delta not 1 or -1";
+  }
+  arlsSlideIndex += delta;
+  if (arlsSlideIndex > arlsSlides.length - 1) {
+    arlsSlideIndex = 0;
+  } else if (arlsSlideIndex < 0) {
+    arlsSlideIndex = arlsSlides.length - 1;
+  }
+  showImage();
 }
 
 function showImage() {
-    for (let i = 0; i < arlsSlides.length; i++) {
-        arlsSlides[i].style.display = "none";
-    }
-    arlsSlides[arlsSlideIndex].style.display = "block";
+  for (let i = 0; i < arlsSlides.length; i++) {
+    arlsSlides[i].style.display = "none";
+  }
+  arlsSlides[arlsSlideIndex].style.display = "block";
 
-    let arlsTitle = document.getElementById("arls-slide-title");
-    arlsTitle.textContent = arlsSlides[arlsSlideIndex].alt;
+  let arlsTitle = document.getElementById("arls-slide-title");
+  arlsTitle.textContent = arlsSlides[arlsSlideIndex].alt;
 }
 
 // Chess demo slides
@@ -56,38 +56,45 @@ let chessSlides = document.getElementsByClassName("chess-slide-video");
 showVideo();
 
 let chessLeftButton = document.querySelector(
-    ".chess-slides .slides-button.left"
+  ".chess-slides .slides-button.left"
 );
 let chessRightButton = document.querySelector(
-    ".chess-slides .slides-button.right"
+  ".chess-slides .slides-button.right"
 );
 chessLeftButton.addEventListener("click", function () {
-    changeVideo(-1);
+  changeVideo(-1);
 });
 chessRightButton.addEventListener("click", function () {
-    changeVideo(+1);
+  changeVideo(+1);
 });
 
 function changeVideo(delta) {
-    if (Math.abs(delta) != 1) {
-        throw "Error: delta not 1 or -1";
-    }
-    chessSlideIndex += delta;
-    if (chessSlideIndex > chessSlides.length - 1) {
-        chessSlideIndex = 0;
-    } else if (chessSlideIndex < 0) {
-        chessSlideIndex = chessSlides.length - 1;
-    }
-    showVideo();
+  if (Math.abs(delta) != 1) {
+    throw "Error: delta not 1 or -1";
+  }
+  chessSlideIndex += delta;
+  if (chessSlideIndex > chessSlides.length - 1) {
+    chessSlideIndex = 0;
+  } else if (chessSlideIndex < 0) {
+    chessSlideIndex = chessSlides.length - 1;
+  }
+  showVideo();
 }
 
 function showVideo() {
-    for (let i = 0; i < chessSlides.length; i++) {
-        chessSlides[i].style.display = "none";
-        chessSlides[i].pause();
-    }
-    chessSlides[chessSlideIndex].style.display = "block";
+  for (let i = 0; i < chessSlides.length; i++) {
+    chessSlides[i].style.display = "none";
+    chessSlides[i].pause();
+  }
+  chessSlides[chessSlideIndex].style.display = "block";
 
-    let title = document.getElementById("chess-slide-title");
-    title.textContent = chessSlides[chessSlideIndex].textContent;
+  let title = document.getElementById("chess-slide-title");
+  title.textContent = chessSlides[chessSlideIndex].textContent;
 }
+
+document.querySelectorAll("h2").forEach((h2) => {
+  h2.onclick = function () {
+    const id = this.parentElement.getAttribute("id");
+    window.location.href = "#" + id;
+  };
+});
--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -3,72 +3,72 @@ import inViewport from "./in-viewport.js";
 //Source: https://css-tricks.com/snippets/css/typewriter-effect/
 
 const TypewriterAnimation = function (el, prefix, toRotate, period) {
-    this.el = el;
-    this.prefix = prefix;
-    this.toRotate = toRotate;
-    this.period = period;
-    this.txt = "";
-    this.loopNum = 0;
-    this.tick();
-    this.isDeleting = false;
+  this.el = el;
+  this.prefix = prefix;
+  this.toRotate = toRotate;
+  this.period = period;
+  this.txt = "";
+  this.loopNum = 0;
+  this.tick();
+  this.isDeleting = false;
 };
 
 TypewriterAnimation.prototype.tick = function () {
-    if (!inViewport(this.el)) {
-        const that = this;
-        setTimeout(function () {
-            that.tick();
-        }, 100);
-        return;
-    }
-
-    let i = this.loopNum % this.toRotate.length;
-    let fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    const span = document.createElement("span");
-    span.innerText = this.prefix + this.txt;
-    span.style.fontSize = "xx-large";
-    this.el.innerHTML = span.outerHTML;
-
-    let delta = 150 - Math.random() * 100;
-
-    if (this.isDeleting) {
-        delta /= 2;
-    }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-
+  if (!inViewport(this.el)) {
     const that = this;
     setTimeout(function () {
-        that.tick();
-    }, delta);
+      that.tick();
+    }, 100);
+    return;
+  }
+
+  let i = this.loopNum % this.toRotate.length;
+  let fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  const span = document.createElement("span");
+  span.innerText = this.prefix + this.txt;
+  span.style.fontSize = "xx-large";
+  this.el.innerHTML = span.outerHTML;
+
+  let delta = 150 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  const that = this;
+  setTimeout(function () {
+    that.tick();
+  }, delta);
 };
 
 const elements = document.getElementsByClassName("typewrite");
 for (const element of elements) {
-    const prefix = element.getAttribute("text-prefix");
-    const toRotate = element.getAttribute("text-array");
-    const period = element.getAttribute("rotate-period");
-    if (toRotate) {
-        new TypewriterAnimation(
-            element,
-            prefix,
-            JSON.parse(toRotate),
-            parseInt(period, 10) || 2000
-        );
-        console.log("Typewriter effect initialized");
-    }
+  const prefix = element.getAttribute("text-prefix");
+  const toRotate = element.getAttribute("text-array");
+  const period = element.getAttribute("rotate-period");
+  if (toRotate) {
+    new TypewriterAnimation(
+      element,
+      prefix,
+      JSON.parse(toRotate),
+      parseInt(period, 10) || 2000
+    );
+    console.log("Typewriter effect initialized");
+  }
 }
