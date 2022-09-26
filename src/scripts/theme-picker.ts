b612565a17f78c65e157131164052d7b372ec08b
David Xu <62825020+dxaviud@users.noreply.github.com>
2022-09-25 20:08:54 -0400

    Convert theme-picker to ts

--- src/scripts/theme-picker.ts
+++ src/scripts/theme-picker.ts
@@ -3,26 +3,35 @@ import aquamarineBackground from "/images/aquamarine-background.jpg";
 import crimsonBackground from "/images/crimson-background.jpg";
 import goldBackground from "/images/gold-background.jpg";
 
-const circles = document.getElementsByClassName("circle");
+const circles = document.getElementsByClassName(
+  "circle"
+) as HTMLCollectionOf<HTMLElement>;
 let currentTheme = circles[0].dataset.theme;
 
 for (let i = 0; i < circles.length; i++) {
   circles[i].addEventListener("click", function () {
-    let theme = this.dataset.theme;
-    if (theme === currentTheme) {
+    const theme = circles[i].dataset.theme;
+    if (theme === currentTheme || theme == undefined) {
       return;
     }
     changeTheme(theme);
-    expandCircle(this);
+    expandCircle(circles[i]);
     normalizeCircle(localStorage.circleIndex);
     localStorage.circleIndex = i;
   });
 }
 
-const root = document.querySelector(":root");
-const welcome = document.getElementById("welcome");
-
-function changeTheme(theme) {
+function changeTheme(theme: string) {
+  const root = document.querySelector(":root") as HTMLElement;
+  const welcome = document.getElementById("welcome");
+  if (root == null) {
+    console.log("couldn't query :root");
+    return;
+  }
+  if (welcome == null) {
+    console.log("couldn't query welcome");
+    return;
+  }
   currentTheme = theme;
   if (theme == "default") {
     root.style.setProperty("--primary-color", "#0395de");
@@ -42,26 +51,34 @@ function changeTheme(theme) {
 const SCALE_FACTOR = 1.2;
 const CIRCLE_SIZE = 25;
 
-function expandCircle(circle) {
+function expandCircle(circle: HTMLElement) {
+  circle.style.transform = "none";
   circle.style.width = `${CIRCLE_SIZE * SCALE_FACTOR}px`;
   circle.style.height = `${CIRCLE_SIZE * SCALE_FACTOR}px`;
 }
 
-function normalizeCircle(index) {
+function normalizeCircle(index: number) {
   circles[index].style.width = `${CIRCLE_SIZE}px`;
   circles[index].style.height = `${CIRCLE_SIZE}px`;
 }
 
 if ("circleIndex" in localStorage) {
-  expandCircle(circles[localStorage.circleIndex]);
-  changeTheme(circles[localStorage.circleIndex].dataset.theme);
+  const circle = circles[localStorage.circleIndex];
+  expandCircle(circle);
+  const theme = circle.dataset.theme;
+  if (theme != undefined) {
+    changeTheme(theme);
+  }
 } else {
   expandCircle(circles[0]);
-  changeTheme(circles[0].dataset.theme);
+  if (circles[0].dataset.theme != undefined) {
+    changeTheme(circles[0].dataset.theme);
+  }
   localStorage.circleIndex = 0;
 }
 
-for (const circle of circles) {
+for (let i = 0; i < circles.length; ++i) {
+  const circle = circles[i];
   circle.addEventListener("mouseover", () => {
     if (circle != circles[localStorage.circleIndex]) {
       circle.style.transform = "scale(1.2)";
