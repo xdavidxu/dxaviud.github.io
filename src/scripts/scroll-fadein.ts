eee101a4e950c64627fc58f0c2bc9dd818d7dde3
David Xu <62825020+dxaviud@users.noreply.github.com>
2022-09-26 19:56:16 -0400

    scroll fadein

---
+++ src/scripts/scroll-fadein.ts
@@ -0,0 +1,29 @@
+export {};
+
+const elements = document.getElementsByClassName(
+  "scroll-fadein"
+) as HTMLCollectionOf<HTMLElement>;
+
+for (let i = 0; i < elements.length; ++i) {
+  const e = elements[i];
+  e.style.opacity = "0";
+  e.style.marginTop = "50px";
+}
+
+const fadein = () => {
+  for (let i = 0; i < elements.length; ++i) {
+    const e = elements[i];
+    const bodyRect = document.body.getBoundingClientRect();
+    const elemRect = e.getBoundingClientRect();
+    const threshold = elemRect.top - bodyRect.top - window.innerHeight / 2;
+    if (document.documentElement.scrollTop > threshold) {
+      e.style.opacity = "1";
+      e.style.transition = "0.7s ease-in";
+      e.style.transform = `translateY(-${e.style.marginTop})`;
+    }
+  }
+};
+
+fadein();
+
+document.addEventListener("scroll", fadein);
